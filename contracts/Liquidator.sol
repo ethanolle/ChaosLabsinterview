5fb1a7e2a279f69e6ae12475b695846ec618d897
ethan.sayagh ethan.sayagh <ethan.sayagh@gmail.com>
2022-12-01 18:28:16 +0100

    created template of the liquidation contract

--- contracts/Liquidator.sol
+++ contracts/Liquidator.sol
@@ -1,12 +1,43 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import {ILendingPool} from "./interfaces/ILendingPool.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Liquidator {
-    function test() public pure returns (uint) {
-        return 1;
+    ILendingPool private immutable lendingPool;
+
+    constructor(address _lendingPool) {
+        lendingPool = ILendingPool(_lendingPool);
+    }
+
+    function liquidation(
+        address collateralAsset,
+        address debtAsset,
+        address user,
+        uint256 debtToCover
+    ) internal returns (uint256) {
+        (, , , , , uint256 healthFactor) = lendingPool.getUserAccountData(user);
+
+        // check health factor
+        require(healthFactor < 1 ether, "health factor too high");
+
+        approveToken(debtAsset, address(lendingPool), debtToCover);
+        lendingPool.liquidationCall(
+            collateralAsset,
+            debtAsset,
+            user,
+            debtToCover,
+            false
+        );
+        uint256 amountOut = IERC20(collateralAsset).balanceOf(address(this));
+        return amountOut;
+    }
+
+    function approveToken(
+        address token,
+        address to,
+        uint256 amountIn
+    ) internal {
+        require(IERC20(token).approve(to, amountIn), "approve failed");
     }
 }
-
